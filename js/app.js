0e98b0adcf6aa1770d02f7896d11c729adfcba8d
AcidKiddo <>
2022-10-15 16:31:19 +0200

    use unminified resource

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
-import * as P2PCF from './libs/dist/p2pcf.min.js';
+import P2PCF from './libs/dist/p2pcf.js';
 
-(function () {
-    const client_id = 'MyUsername';
-    const room_id = 'MyRoom';
-    window.P2PCF = P2PCF;
-    const p2pcf = new P2PCF('abc', '123');
-    console.log('p2pcf', p2pcf);
+(() => {
+    let loc_hash = window.location.hash;
+    if (!loc_hash) {
+        console.error('Missing hash specs');
+    } else {
+        const client_id = 'MyUsername';
+        const room_id = loc_hash;
+        const p2pcf = new P2PCF(client_id, room_id);
+        console.log('loc_hash room_id', loc_hash);
+        window.p2pcf = p2pcf;
+    }
 })();
