13d76e5ffde0fb2170e6c9fbfce81cdce1d4061e
AcidKiddo <>
2022-10-15 16:18:12 +0200

    minor update [wip]

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-console.log('hello world!');
+import * as P2PCF from './libs/dist/p2pcf.min.js';
+
+(function () {
+    const client_id = 'MyUsername';
+    const room_id = 'MyRoom';
+    window.P2PCF = P2PCF;
+    const p2pcf = new P2PCF('abc', '123');
+    console.log('p2pcf', p2pcf);
+})();
