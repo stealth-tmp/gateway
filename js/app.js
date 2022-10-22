a9fc524baba04b33798c60c55c4e9880b1efa2dd
AcidKiddo <>
2022-10-22 12:19:03 +0200

    add module for p2pcf & minor change in code

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-import P2PCF from './libs/dist/p2pcf.js';
+import P2PCF from './modules/p2pcf.js';
 
 (() => {
     let loc_hash = window.location.hash;
     if (!loc_hash) {
-        console.error('Missing hash specs');
-    } else {
-        const client_id = 'MyUsername';
-        const room_id = loc_hash;
-        const p2pcf = new P2PCF(client_id, room_id);
-        console.log('loc_hash room_id', loc_hash);
-        window.p2pcf = p2pcf;
+        console.error('Missing hash specs, using test room hash');
+        loc_hash = 'test';
     }
+    const client_id = 'MyUsername';
+    const room_id = loc_hash;
+    const p2pcf = new P2PCF(client_id, room_id);
+    console.log('loc_hash room_id', loc_hash);
+    window.p2pcf = p2pcf;
 })();
